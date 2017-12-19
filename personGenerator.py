1f8440d344ce73d62ff7cd90edf972f15154adb4
Amar Z <amar.zlojic@gmail.com>
2017-12-19 15:09:39 +0100

    Update personGenerator.py

--- personGenerator.py
+++ personGenerator.py
@@ -49,21 +49,102 @@ def GetPhoneNumber(size):
     phone += '\"'
     return phone
 
-def WriteToFile(start, end):
+def GetEmail(name, surname):
+    providers = ['gmail.com', 'yahoo.com', 'hotmail.com',
+            'outlook.com', 'edu.fit.ba', 'mail.com', 
+            'protonmail.com', 'aol.com', 'yandex.com', 'zoho.com']
+    
+    email = '"' + name + surname + '@' + \
+            providers[random.randint(0, len(providers)) - 1] + \
+            '"'
+
+    return email
+
+def GetPassword():
+    password = '"'
+    password += 'password'
+    for i in range(8):
+        password += random.choice(string.digits)
+
+    password += '"'
+    return password
+
+def GetUsername(name, surname):
+    username = '"' + name + surname
+    for i in range(3):
+        username += random.choice(string.digits)
+
+    username += '"'
+    return username
+
+def GetRandomDate(lowYear = 2015, highYear = 2017):
+    day = str(random.randint(1, 28))
+    day = day.rjust(2, '0')
+    month = str(random.randint(1, 12))
+    month = month.rjust(2, '0')
+
+    date = str(random.randint(lowYear, highYear))
+    date += month
+    date += day
+
+    return date
+
+def ClearFile(fileName):
+    f = open(fileName, 'w')
+    f.write('')
+    f.close()
+
+def WriteToFileLogin(loginID, name, surname):
+    name = name[1:-1]
+    surname = surname[1:-1]
+
+    line = str(loginID) + ','
+    line += GetEmail(name, surname) + ','
+    line += GetUsername(name, surname) + ','
+    line += GetPassword() + ','
+    line += 'Yes,'
+    line += '#PlaceholderForPriority#'
+
+    f = open("login.txt", "a")
+
+    f.write(line)
+    f.write('\n')
+
+    f.close()
+
+def WriteToFileClients(loginID):
+    ccn = '"'
+    ccn += fake.credit_card_number()
+    ccn += '"'
+
+    client = loginID + ','
+    client += ccn + ','
+    client += GetRandomDate()
+
+    f = open("clients.txt", "a")
+
+    f.write(client)
+    f.write('\n')
+
+    f.close()
+
+def WriteToFilePersons(start, end):
     lines = []
-    line = ''
+    ClearFile('login.txt')
+    Clearfile('clients.txt')
+
     for i in range(start, end):
-        line = ''
-        line += str(i)
-        line += ','
-        line += str(random.randint(1,3173959))
-        line += ','
-        line += GetFirstName()
-        line += ','
-        line += GetLastName()
-        line += ','
-        line += GetPhoneNumber(9)
-        line += ','
+        name = GetFirstName()
+        surname = GetLastName()
+
+        WriteToFileLogin(i, name, surname)
+        WriteToFileClients(i)
+
+        line = str(i) + ','
+        line += str(random.randint(1,3173959)) + ','
+        line += name + ','
+        line += surname + ','
+        line += GetPhoneNumber(9) + ','
         line += GetAddress()
         lines.append(line)
 
@@ -76,5 +157,7 @@ def WriteToFile(start, end):
     f.close()
 
 if __name__ == "__main__":
-    WriteToFile(1, 11)
-    #GetAddress()
+    startingID = int(input("Enter the starting ID ---> "))
+    endingID = int(input("Enter the ending ID ---> "))
+    endingID += 1
+    WriteToFilePersons(startingID, endingID)  
